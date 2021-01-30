4ac4387cfde88084e5b1e5b0244d9ea216eacc89
g-harel <gabrielj.harel@gmail.com>
2021-01-30 13:26:43 -0600

    refactor tag matchers

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import {tagTransactions} from "./internal/tags";
 import {filter, print, sum} from "./internal/transaction";
 
 // TODO
-// Deduplicate transactions in close time range and same absolute amount.
+// Deduplicate transactions in close time range and same absolute amount. (paypal)
 // Merge transactions from multiple sources (by similarity?).
 // Filter with OR/NOT/AND logic.
 
@@ -12,7 +12,7 @@ const transactions = tagTransactions(
     readMintArchive(".transactions.csv"),
 );
 
-const tag = "taxes";
+const tag = "utility";
 filter(transactions, [tag]).map((t) => console.log(print(t)));
 console.log("====");
 console.log(tag, sum(filter(transactions, [tag])).toLocaleString());
