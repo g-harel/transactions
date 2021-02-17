08ea3ce5643dd3ec48ebdbd19b6e0ee45a6ddfd1
g-harel <gabrielj.harel@gmail.com>
2021-02-16 23:58:10 -0600

    start implementing tag tree logic

--- index.ts
+++ index.ts
@@ -1,20 +1,27 @@
 import {tagTransactions} from "./internal/match";
 import {dedupe} from "./internal/uniq";
-import {filter, sort, sum} from "./internal/query";
+import {filter, sort, sum, tagTree} from "./internal/query";
 import {printTransaction} from "./internal/transaction";
 import {slurp} from "./internal/slurp";
 import {argv} from "./internal/cli";
+import {logInfo} from "./internal/log";
 
 // TODO query with OR/NOT/AND logic.
 // TODO store converted format in file.
+// TODO add commands to cli
 
-const transactions = dedupe(tagTransactions(argv.matchfile, slurp(argv.dir)));
+if (true) {
+    const transactions = tagTransactions(argv.matchfile, slurp(argv.dir));
+    logInfo("tags", tagTree(transactions));
+}
 
 if (false) {
+    const transactions = dedupe(
+        tagTransactions(argv.matchfile, slurp(argv.dir)),
+    );
     const tag = "food";
     sort(filter(transactions, [tag])).map((t) =>
-        console.log(printTransaction(t)),
+        logInfo(printTransaction(t).replace("\n", " ")),
     );
-    console.log("====");
-    console.log(tag, sum(filter(transactions, [tag])).toLocaleString());
+    logInfo(tag, sum(filter(transactions, [tag])).toLocaleString());
 }
--- internal/query.ts
+++ internal/query.ts
@@ -1,3 +1,4 @@
+import {logInfo} from "./log";
 import {MatchedTransaction} from "./match";
 import {Transaction} from "./transaction";
 
@@ -31,4 +32,74 @@ export const sum = (transactions: Transaction[]): number => {
 
 export const sort = <T extends Transaction>(transactions: T[]): T[] => {
     return transactions.sort((a, b) => a.date.getTime() - b.date.getTime());
+};
+
+export const tagTree = (transactions: MatchedTransaction[]): string => {
+    const tagCombos: Record<string, Record<string, boolean>> = {};
+    const tagFrequency: Record<string, number> = {};
+    const tagComboFrequency: Record<string, number> = {};
+    for (const transaction of transactions) {
+        for (const tag of transaction.matcher.tags) {
+            if (tagFrequency[tag] === undefined) {
+                tagFrequency[tag] = 0;
+            }
+            tagFrequency[tag]++;
+
+            for (const otherTag of transaction.matcher.tags) {
+                if (tag === otherTag) continue;
+
+                if (tagComboFrequency[tag] === undefined) {
+                    tagComboFrequency[tag] = 0;
+                }
+                tagComboFrequency[tag]++;
+
+                if (tagCombos[tag] === undefined) {
+                    tagCombos[tag] = {};
+                }
+                tagCombos[tag][otherTag] = true;
+            }
+        }
+    }
+    logInfo("tagCombos", JSON.stringify(tagCombos, null, 2));
+
+    const tags = Object.keys(tagCombos);
+    const tagCount = tags.length;
+    const orderedTags = Object.entries(tagComboFrequency)
+        .sort((a, b) => b[1] - a[1])
+        .map(([tag, count]) => ({tag, count}));
+
+
+    logInfo("orderedTags", JSON.stringify(orderedTags, null, 2));
+
+    interface TagTree {
+        tag: string;
+        children: Record<string, TagTree>;
+    }
+
+    const rootTag: TagTree = {
+        tag: "root",
+        children: {
+            [orderedTags[0].tag]: {
+                tag: orderedTags[0].tag,
+                children: {},
+            },
+        },
+    };
+    // TODO make recursive.
+    for (let i = 0; i < tagCount; i++) {
+        const {tag} = orderedTags[i];
+        let found = false;
+        for (const siblingTag of Object.keys(rootTag.children)) {
+            console.log(tag, siblingTag);
+            if (tagCombos[tag][siblingTag]) {
+                found = true;
+                break;
+            };
+        }
+        if (found) break;
+        rootTag.children[tag] = {tag, children: {}};
+    }
+    console.log(rootTag);
+
+    return "";
 };
