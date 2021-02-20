e18a18f95d952a1816ae750954873e65d73a22b4
g-harel <gabrielj.harel@gmail.com>
2021-02-19 23:09:08 -0600

    add cli command to print tag tree

--- internal/cli.ts
+++ internal/cli.ts
@@ -1,5 +1,13 @@
 import yargs from "yargs/yargs";
 import {hideBin} from "yargs/helpers";
+import {tagTransactions} from "./match";
+import {slurp} from "./slurp";
+import {logInfo} from "./log";
+import {tagTree} from "./query";
+
+// TODO query with OR/NOT/AND logic.
+// TODO store converted format in file.
+// TODO add commands to cli
 
 export const argv = yargs(hideBin(process.argv))
     .option("verbose", {
@@ -18,4 +26,17 @@ export const argv = yargs(hideBin(process.argv))
         type: "string",
         description: "Specify matchfile",
         demandOption: true,
-    }).argv;
+    })
+    .command(
+        "tag-tree",
+        "Print the calculated tag tree",
+        () => {},
+        (argv) => {
+            console.log("gasdf");
+            const transactions = tagTransactions(
+                argv.matchfile,
+                slurp(argv.dir),
+            );
+            logInfo("tags", tagTree(transactions));
+        },
+    ).argv;
